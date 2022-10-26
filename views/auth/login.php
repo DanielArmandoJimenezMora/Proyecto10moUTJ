61a2c6bf5e41e17a744cee6545fb007e2c53e498
Daniel Jimenez Mora <danieljimenezmora@gmail.com>
2022-10-26 02:12:44 -0500

    Login actualizado con background movil

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,39 +1,40 @@
 <main class="auth">
     
-    <img src="../build/img/logo.svg" alt="logo" class="auth__logo">
-    <h2 class="auth__heading"><?= $titulo ?></h2>
-    <p class="auth__texto">Inicia sesión en <b>Ticket Reserve</b></p>
+    <div class="login-card">
+        <img src="../build/img/logo.svg" alt="logo" class="auth__logo">
+        <h2 class="auth__heading"><?= $titulo ?></h2>
+        <p class="auth__texto">Inicia sesión en <b>Ticket Reserve</b></p>
 
-    <form class="formulario">
-        <div class="formulario__campo">
-            <label for="email" class="formulario__label">Email</label>
-            <input 
+        <form class="formulario">
+            <div class="formulario__campo">
+                <label for="email" class="formulario__label">Email</label>
+                <input 
                     type="text"
                     class="formulario__input"
                     placeholder="Tu Email"
                     id="email"
                     name="email"
                 >
-        </div>
+            </div>
 
-        <div class="formulario__campo">
-            <label for="password" class="formulario__label">Password</label>
-            <input 
+            <div class="formulario__campo">
+                <label for="password" class="formulario__label">Password</label>
+                <input 
                     type="password"
                     class="formulario__input"
                     placeholder="Tu Password"
                     id="password"
                     name="password"
                 >
+            </div>
+
+            <input type="submit" class="formulario__submit" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Obtener una</a>
+            <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
         </div>
-
-        <input type="submit" class="formulario__submit" value="Iniciar Sesión">
-
-    </form>
-
-    <div class="acciones">
-        <a href="/registro" class="acciones__enlace">¿Aún no tienes cuenta? Obtener una</a>
-        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
     </div>
 
 </main>
--- views/auth/registro.php
+++ views/auth/registro.php
@@ -1,71 +1,72 @@
 <main class="auth">
-    <img src="../build/img/logo.svg" alt="logo" class="auth__logo">
-    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <div class="login-card">
+        <img src="../build/img/logo.svg" alt="logo" class="auth__logo">
+        <h2 class="auth__heading"><?php echo $titulo; ?></h2>
 
-    <form class="formulario">
+        <form class="formulario">
 
-     <div class="formulario__campo">
-            <label for="nombre" class="formulario__label">Nombre</label>
-            <input 
-                    type="text"
-                    class="formulario__input"
-                    placeholder="Nombre"
-                    id="nombre"
-                    name="nombre"
-                >
+            <div class="formulario__campo">
+                <label for="nombre" class="formulario__label">Nombre</label>
+                <input 
+                        type="text"
+                        class="formulario__input"
+                        placeholder="Nombre"
+                        id="nombre"
+                        name="nombre"
+                    >
+            </div>
+
+            <div class="formulario__campo">
+                <label for="apellido" class="formulario__label">Apellido</label>
+                <input 
+                        type="text"
+                        class="formulario__input"
+                        placeholder="Apellido"
+                        id="apellido"
+                        name="apellido"
+                    >
+            </div>
+
+            <div class="formulario__campo">
+                <label for="email" class="formulario__label">Email</label>
+                <input 
+                        type="email"
+                        class="formulario__input"
+                        placeholder="Email"
+                        id="email"
+                        name="email"
+                    >
+            </div>
+
+            <div class="formulario__campo">
+                <label for="password" class="formulario__label">Password</label>
+                <input 
+                        type="password"
+                        class="formulario__input"
+                        placeholder="Password"
+                        id="password"
+                        name="password"
+                    >
+            </div>
+
+            <div class="formulario__campo">
+                <label for="password2" class="formulario__label">Repetir Password</label>
+                <input 
+                        type="password"
+                        class="formulario__input"
+                        placeholder="Repetir Password"
+                        id="password2"
+                        name="password2"
+                    >
+            </div>
+
+            <input type="submit" class="formulario__submit" value="Crear Cuenta">
+
+        </form>
+
+        <div class="acciones">
+            <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Iniciar Sesión</a>
+            <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
         </div>
-
-         <div class="formulario__campo">
-            <label for="apellido" class="formulario__label">Apellido</label>
-            <input 
-                    type="text"
-                    class="formulario__input"
-                    placeholder="Apellido"
-                    id="apellido"
-                    name="apellido"
-                >
-        </div>
-
-        <div class="formulario__campo">
-            <label for="email" class="formulario__label">Email</label>
-            <input 
-                    type="email"
-                    class="formulario__input"
-                    placeholder="Email"
-                    id="email"
-                    name="email"
-                >
-        </div>
-
-        <div class="formulario__campo">
-            <label for="password" class="formulario__label">Password</label>
-            <input 
-                    type="password"
-                    class="formulario__input"
-                    placeholder="Password"
-                    id="password"
-                    name="password"
-                >
-        </div>
-
-         <div class="formulario__campo">
-            <label for="password2" class="formulario__label">Repetir Password</label>
-            <input 
-                    type="password"
-                    class="formulario__input"
-                    placeholder="Repetir Password"
-                    id="password2"
-                    name="password2"
-                >
-        </div>
-
-        <input type="submit" class="formulario__submit" value="Crear Cuenta">
-
-    </form>
-
-    <div class="acciones">
-        <a href="/login" class="acciones__enlace">¿Ya tienes cuenta? Iniciar Sesión</a>
-        <a href="/olvide" class="acciones__enlace">¿Olvidaste tu password?</a>
     </div>
-
 </main>
